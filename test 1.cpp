8790f800111186d7dec53d7aed8d11a5cb7e071a
930727fre <44873877+Freddy930727@users.noreply.github.com>
2021-05-31 19:48:01 +0800

    Update test 1.cpp

--- test 1.cpp
+++ test 1.cpp
@@ -1,18 +1,112 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
+<h1 class="block-effect" style="--td: 1.2s">
+   <div class="block-reveal" style="--bc: #116cd4; --d: .1s">Welcome to</div>
+   <div class="block-reveal" style="--bc: #8611d4; --d: .5s">yphs302</div>
+</h1>
 
+<div class="container">
+   <a target="_blank">
+      <img src="https://i.imgur.com/C1bJ2SY.jpg" alt="welcome!"/>
+      <div class="centered">歡迎光臨!
+      </div>
+   </a>
+</div>
 
-int main(void)
-{
-int num,numptr;
-int* ptr1;
-num=1;
-ptr1=&num;
-cout<<*ptr1;
+關於我們班
 
+<div class="container">
+   <a target="_blank">
+      <img src="https://upload.cc/i1/2021/05/29/qgcUv0.jpg"/>
+      <div class="centered">About us</div>
+   </a>
+</div>
 
-   return 0;
-}
+高三二班是一個活潑的班級，也是歷史上第一個受 108 課綱荼毒的班級。
+我們在導師的督促下，即使全盛時期曾有六位籃球隊隊員，教室總是整潔乾淨、沒有異味，而且我們班雖擁有多位午評，卻沒有做過假公濟私的行為，可見三零二班之優秀。
+在秩序方面，雖然偶有吵鬧的時候，但更常被鄭慧君老師稱讚是一個積極可愛的班級（？！）。
+而我們這個人才濟濟的班級的信念就是 : 努力苦讀考好學測，並且好好享受在延平的高中生活 !
 
+![](https://hbl917070.cf/img/show?img=jojo_4&imgw=900&size=40&tc=ffffffff&tbt=6&tb=000000ff&tbac=00000000&y=100&txt=%E8%B7%9D%E9%9B%A2%E5%AD%B8%E6%B8%AC%E4%BD%A0%E9%82%84%E5%89%A9%E4%B8%8B%EF%BD%9Bbr%EF%BD%9D%EF%BD%9B%E8%B7%9D%E9%9B%A2%E7%9B%AE%E5%89%8D%E6%99%82%E9%96%93(2022%2F1%2F23%208%3A00%3A00)%EF%BD%9D%EF%BD%9Bbr%EF%BD%9D#.jpg)
+
+班級介紹影片
+
+<center><iframe width="560" height="315" src="https://www.youtube.com/embed/9wxzLJCjxaQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+
+<div class="container">
+   <a target="_blank">
+      <img src="https://upload.cc/i1/2021/05/29/yUdIAY.jpg" />
+      <div class="centered">老師及幹部介紹</div>
+   </a>
+</div>
+
+<div class="row2">
+   <div class="column2">
+      <img src="https://upload.cc/i1/2021/05/29/Xfr9ZO.jpg" alt="Snow" style="width:100%">
+   </div>
+   <div class="column2">
+      <img src="https://upload.cc/i1/2021/05/29/Xfr9ZO.jpg" alt="Forest" style="width:100%">
+   </div>
+</div>
+
+<div class="container">
+   <a target="_blank">
+      <img src="https://upload.cc/i1/2021/05/29/yUdIAY.jpg"/>
+      <div class="centered">班級活動</div>
+   </a>
+</div>
+
+<div class="row2">
+   <div class="column2">
+      <img src="https://upload.cc/i1/2021/05/29/Xfr9ZO.jpg" alt="Snow" style="width:100%">
+   </div>
+   <div class="column2">
+      <img src="https://upload.cc/i1/2021/05/29/glAhQY.jpg" alt="Forest" style="width:100%">
+   </div>
+</div>
+
+籃球 校慶
+
+<div class="containerimg">
+   <div class="boximg">
+      <img src="https://source.unsplash.com/1000x800">
+      <span>Image</span>
+   </div>
+   <div class="boximg">
+      <img src="https://source.unsplash.com/1000x802">
+      <span>Image</span>
+   </div>
+   <div class="boximg">
+      <img src="https://source.unsplash.com/1000x804">
+      <span>Image</span>
+   </div>
+   <div class="boximg">
+      <img src="https://source.unsplash.com/1000x806">
+      <span>Image</span>
+   </div>
+</div>
+
+<div class="row3">
+   <div class="column3">
+      <img src="https://upload.cc/i1/2021/05/29/jctSEk.jpg" style="width:100%">
+   </div>
+   <div class="column3">
+      <img src="https://upload.cc/i1/2021/05/29/glAhQY.jpg" style="width:100%">
+   </div>
+   <div class="column3">
+      <img src="https://upload.cc/i1/2021/05/29/8iJ7dw.jpg" style="width:100%">
+   </div>
+</div>
+
+StackEdit stores your files in your browser, which means all your files are automatically saved locally and are accessible **offline!**
+[site](/yphs302/teachers/ "site")
+
+學校地址
+台北市大安區建國南路一段275號
+電話:02 2707 1478
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.928791059507!2d121.5370088140527!3d25.036490544347743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abd5bf8b64b7%3A0xc4253740116ead73!2z5Y-w5YyX5biC56eB56uL5bu25bmz6auY57Sa5Lit5a24!5e0!3m2!1szh-TW!2stw!4v1622271913370!5m2!1szh-TW!2stw" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+<div class="wrappertyping">
+   <div class="typing-demo">
+      New posts are being edited,please stay tuned!
+   </div>
+</div>
